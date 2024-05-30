69ea90a81f6db4d07e09a1fd7411fcea06458a63
KishoreBabu <kishore.sai37@gmail.com>
2024-05-30 15:58:02 +0530

    add html structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//elements
+const productList = document.getElementById('product-listing');
+const loadMoreButton = document.getElementById('load-more');
+const searchBar = document.getElementById('search-bar');
+const categoryFilter = document.getElementById('category-filter');
+const priceFilter = document.getElementById('price-filter');
+const availabilityFilter = document.getElementById('availability-filter');
